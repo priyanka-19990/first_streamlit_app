9cfb4d9aa266b4aa6a92580b4a237d6910c2ea1b
priyanka-19990 <105282931+priyanka-19990@users.noreply.github.com>
2023-08-10 13:29:28 +0530

    Update Streamlit_app.py
    import pandas

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -13,3 +13,6 @@ st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 # Add a new header
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas as p
+my_fruit_list = p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
+st.dataframe(my_fruit_list)
