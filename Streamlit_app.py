78ecbfe26b008326d3917a790b4d113c60a06e37
priyanka-19990 <105282931+priyanka-19990@users.noreply.github.com>
2023-08-10 13:48:31 +0530

    Update Streamlit_app.py
    Pick up fruit

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -17,6 +17,7 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas as p
 # Passing txt file in a variable
 my_fruit_list = p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
+my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include
 st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # Display the table on the page
