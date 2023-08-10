9b96bc92b7eb043674b643516d210791f8d1d474
priyanka-19990 <105282931+priyanka-19990@users.noreply.github.com>
2023-08-10 12:33:15 +0530

    Update Streamlit_app.py
    content added

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,15 +1,11 @@
 import streamlit as st
-
-st.title('My Parents New Healthy Diner')
-
 # Add a title
-st.title("My Streamlit App")
-
-# Add text
-st.write("Welcome to my Streamlit app!")
-
-# Add an interactive widget (e.g., a slider)
-slider_value = st.slider("Select a value", 0, 100)
-
-# Display the selected value
-st.write("You selected:", slider_value)
+st.title('My Parents New Healthy Diner')
+# Add a header
+st.header('Breakfast Menu')
+# Add a text
+st.text('Omega 3 & Blueberry Oatmeal')
+# Add a text
+st.text('Kale, Spinach & Rocket Smoothie')
+# Add a text
+st.text('Hard-Boiled Free-Range Egg')
